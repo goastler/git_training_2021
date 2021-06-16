a89e93a1f2929097ac246b337892096a93659019
James <jim007.atkinson@hotmail.co.uk>
2021-06-16 10:41:34 +0100

    james

--- setup.sh
+++ setup.sh
@@ -1,15 +1,10 @@
 #! /bin/bash
 
 # print your name below, i.e. echo "george"
-<<<<<<< HEAD
-echo "james"
-echo "Niklas"
-echo "Ben"
-=======
 echo "dominic"
 echo "Niklas"
 echo "Ben"
 echo "Benji"
 echo "Lauren"
+echo "james"
 
->>>>>>> 3d70b70b95887371fe9a117b5455a5774d1b9139
