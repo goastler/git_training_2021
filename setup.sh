6afe9999d7976cfe57b37256643d2bedfa6eca89
Will Vickers <w.vickers@uea.ac.uk>
2021-06-16 10:44:03 +0100

    Will's commit

--- setup.sh
+++ setup.sh
@@ -1,15 +1,10 @@
 #! /bin/bash
 
 # print your name below, i.e. echo "george"
-<<<<<<< HEAD
-echo "james"
-echo "Niklas"
-echo "Ben"
-=======
 echo "dominic"
 echo "Niklas"
 echo "Ben"
 echo "Benji"
 echo "Lauren"
+echo "Will"
 
->>>>>>> 3d70b70b95887371fe9a117b5455a5774d1b9139
